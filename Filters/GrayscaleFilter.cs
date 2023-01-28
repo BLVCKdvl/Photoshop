6ba025437b99ec828b544814250ee6827792d564
XD <s20.rex9000@gmail.com>
2023-01-28 15:38:00 +0300

    added black/white filter

---
+++ Filters/GrayscaleFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyPhotoshop
+{
+    public class GrayscaleFilter : IFilter
+    {
+        public ParameterInfo[] GetParameters()
+        {
+            return new[]
+            {
+                new ParameterInfo { Name="Коэффициент", MaxValue=0, MinValue=0, Increment=0.1, DefaultValue=1 }
+            };
+        }
+
+        public override string ToString()
+        {
+            return "черно/белый формат";
+        }
+
+        public Photo Process(Photo original, double[] parameters)
+        {
+            var result = new Photo(original.width, original.height);
+
+            for (int x = 0; x < result.width; x++)
+            {
+                for (int y = 0; y < result.height; y++)
+                {
+                    result[x, y] = new Pixel
+                    {
+                        R = original[x, y].R,
+                        G = original[x, y].R,
+                        B = original[x, y].R
+                    };
+                }
+            }
+
+            return result;
+        }
+    }
+}
