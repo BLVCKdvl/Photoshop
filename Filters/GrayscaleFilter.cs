8367a1dc51487cd64c66f20880f500c1d33ab2f0
XD <s20.rex9000@gmail.com>
2023-01-29 21:08:26 +0300

    added abstract class PixelFilter

--- Filters/GrayscaleFilter.cs
+++ Filters/GrayscaleFilter.cs
@@ -2,37 +2,22 @@
 
 namespace MyPhotoshop
 {
-    public class GrayscaleFilter : IFilter
+    public class GrayscaleFilter : PixelFilter
     {
-        public ParameterInfo[] GetParameters()
+        public override ParameterInfo[] GetParameters()
         {
             return new ParameterInfo[0];
+        }
+
+        public override Pixel ProcessPixel(Pixel original, double[] parameters)
+        {
+            var lightness = original.R * 0.2126 + original.G * 0.7152 + original.B * 0.0722;
+            return new Pixel(lightness, lightness, lightness);
         }
 
         public override string ToString()
         {
             return "черно/белый формат";
-        }
-
-        public Photo Process(Photo original, double[] parameters)
-        {
-            var result = new Photo(original.width, original.height);
-
-            for (int x = 0; x < result.width; x++)
-            {
-                for (int y = 0; y < result.height; y++)
-                {
-                    result[x, y] = ProcessPixel(original[x, y], parameters);
-                }
-            }
-
-            return result;
-        }
-
-        public Pixel ProcessPixel (Pixel original, double[] parameters)
-        {
-            var lightness = original.R * 0.2126 + original.G * 0.7152 + original.B * 0.0722;
-            return new Pixel(lightness, lightness, lightness);
         }
     }
 }
